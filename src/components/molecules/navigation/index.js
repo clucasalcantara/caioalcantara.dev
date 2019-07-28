be476f68303eb0133e46245575c6b2737f53f1ba
Caio Alcantara <alcantaracaiolucas@gmail.com>
2019-07-28 20:18:00 +0100

    feature(active-page): Add toggle content and change before to after

--- src/components/molecules/navigation/index.js
+++ src/components/molecules/navigation/index.js
@@ -4,30 +4,42 @@
  */
 import React, { useState } from 'react'
 import styled from '@emotion/styled'
+// Helpers
+import ReactInterval from 'react-interval'
 
 const Wrapper = styled.nav({
   display: 'block'
 })
 
-const NavItem = styled.a(({ active, darkMode, theme: { colors } }) => ({
-  fontSize: '1.2em',
-  textDecoration: 'none',
-  color: darkMode ? colors.dark : colors.light,
-  textTransform: 'uppercase',
-  display: 'block',
-  marginBottom: '.5em',
-  '::before': {
-    content: active ? '"▋"' : '""'
-  }
-}))
+const NavItem = styled.a(
+  ({ active, darkMode, theme: { colors }, afterContent }) => ({
+    fontSize: '1.2em',
+    textDecoration: 'none',
+    color: darkMode ? colors.dark : colors.light,
+    textTransform: 'uppercase',
+    display: 'block',
+    marginBottom: '.5em',
+    '::after': {
+      marginLeft: '.2rem',
+      content: active ? afterContent : '""'
+    }
+  })
+)
 
 const Navigation = ({ active = 0, navItems, darkMode, theme }) => {
   const [activeItem, setActiveItem] = useState(active)
+  const [afterContent, setAfterContent] = useState('"▋"')
 
   return (
     <Wrapper>
+      <ReactInterval
+        timeout={1000}
+        enabled
+        callback={() => setAfterContent(afterContent === '"▋"' ? '"_"' : '"▋"')}
+      />
       {navItems.map(({ id, text, path }, idx) => (
         <NavItem
+          afterContent={afterContent}
           theme={theme}
           darkMode={darkMode}
           key={id}
